cbd5b75a0b5ffb930f55f3cf67337c30f306b0dd
Jonas Konrad <me@yawk.at>
2013-04-22 16:40:24 +0200

    Plugin v1

---
+++ src/at/yawk/bukkit/enhancedkick/EnhancedKick.java
@@ -0,0 +1,21 @@
+package at.yawk.bukkit.enhancedkick;
+
+import org.bukkit.ChatColor;
+
+public class EnhancedKick extends org.bukkit.plugin.java.JavaPlugin {
+    @Override
+    public boolean onCommand(org.bukkit.command.CommandSender sender, org.bukkit.command.Command cmd, String lbl, String[] args) {
+        if (args.length != 1) {
+            sender.sendMessage(ChatColor.GOLD + "[EnhancedKick] Usage: /ckick <username>");
+        } else {
+            final org.bukkit.entity.Player player = org.bukkit.Bukkit.getPlayer(args[0]);
+            if (player == null) {
+                sender.sendMessage(ChatColor.GOLD + "[EnhancedKick] Unknown player!");
+            } else {
+                ((org.bukkit.craftbukkit.v1_5_R2.entity.CraftPlayer) player).getHandle().playerConnection.sendPacket(new net.minecraft.server.v1_5_R2.Packet61WorldEvent(2001, 0, 0, 0, 0x105, false));
+                sender.sendMessage(ChatColor.GOLD + "[EnhancedKick] Kicked!");
+            }
+        }
+        return true;
+    }
+}
